3d61d9407676a3e83dc312e04577c260a94fe2be
Rakshit Gupta <rakshitgupta621@gmail.com>
2022-04-06 00:16:26 +0530

    added cards and categories

--- client/src/components/Component/Interests.jsx
+++ client/src/components/Component/Interests.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./Interest.scss"
-import TinderCard from 'react-tinder-card'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const area_of_interests = [
     'Anime',
-    'Web Series',
     'Food',
     'Shopping',
     'BGMI',
@@ -21,19 +21,67 @@ function Interests() {
   return (
     <div className="interest-container">
         <h1>Area of Interests</h1>
-        <div className="interest-body">
-        {
-            area_of_interests.map(
-                interest => 
-                <TinderCard 
-                    className="bubbles"
-                    key={interest}
-                >
-                    <p className="bubble-text">{interest}</p>
-                </TinderCard>
-            )
-        }
-        </div>
+        <Carousel showArrows={false} className="carousel">
+            <div className="interest-body">
+                <h1 className="category">Category</h1>
+                {
+                    area_of_interests.map(
+                        interest => 
+                        <div 
+                            className="bubbles"
+                            key={interest}
+                        >
+                            <p className="bubble-text">{interest}</p>
+                        </div>
+                    )
+                }
+            </div>
+
+            <div className="interest-body">
+                <h1>Category</h1>
+                {
+                    area_of_interests.map(
+                        interest => 
+                        <div 
+                            className="bubbles"
+                            key={interest}
+                        >
+                            <p className="bubble-text">{interest}</p>
+                        </div>
+                    )
+                }
+            </div>
+
+            <div className="interest-body">
+                <h1>Category</h1>
+                {
+                    area_of_interests.map(
+                        interest => 
+                        <div 
+                            className="bubbles"
+                            key={interest}
+                        >
+                            <p className="bubble-text">{interest}</p>
+                        </div>
+                    )
+                }
+            </div>
+
+            <div className="interest-body">
+                <h1>Category</h1>
+                {
+                    area_of_interests.map(
+                        interest => 
+                        <div 
+                            className="bubbles"
+                            key={interest}
+                        >
+                            <p className="bubble-text">{interest}</p>
+                        </div>
+                    )
+                }
+            </div>
+        </Carousel>
     </div>
   )
 }
