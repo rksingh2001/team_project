da8edcbf44950c86aa3ea4907e775d33f91818c0
rohit gupta <r04itgupta@gmail.com>
2022-05-30 23:42:40 +0530

    firebase setup and dotenv creation

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -1,1 +1,19 @@
-// Firebase
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: "team-project-11.firebaseapp.com",
+  databaseURL: "https://team-project-11-default-rtdb.firebaseio.com",
+  projectId: "team-project-11",
+  storageBucket: "team-project-11.appspot.com",
+  messagingSenderId: "4356558927",
+  appId: "1:4356558927:web:6d16816625464ab089da3a",
+  measurementId: "G-B7M8GEVZ6P"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
